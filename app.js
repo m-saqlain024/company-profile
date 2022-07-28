bf67d11a8584c961678080314c2aaf5d0daf9c0e
BeingMurshid <murshid71704@gmail.com>
2022-07-28 05:45:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("week 06")
+
+
+let  num1 = 12;
+let num2 = 2 ;
+// let result  = num1 / num2;
+// let result  = num1 % num2;  module of the scrit
+// let result  = num1 ** num2;  //find the power 
+let result  = num1 / num2;
+
+
+console.log(result)
+
+
+
